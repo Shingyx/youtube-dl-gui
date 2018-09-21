a479797dd8ddce109f98be0ca7c0f8c0482af0a3
Su-Shing Chen <Shingyx@users.noreply.github.com>
2018-09-22 00:18:06 +1200

    Quit properly

--- src/main/main.ts
+++ src/main/main.ts
@@ -16,11 +16,14 @@ app.on('ready', () => {
             url.format({
                 pathname: path.join(__dirname, 'index.html'),
                 protocol: 'file',
-                slashes: true,
             }),
         );
     }
     mainWindow.on('closed', () => {
         mainWindow = undefined;
     });
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
 });
